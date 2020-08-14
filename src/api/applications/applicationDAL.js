d18b1c7d41c0bdd406d643770db73f5c0e13f4b8
Gil Herr <gilherr@gmail.com>
2020-08-14 19:21:25 +0300

    remove usage of axios params as it doubles latency

--- src/api/applications/applicationDAL.js
+++ src/api/applications/applicationDAL.js
@@ -7,19 +7,21 @@ async function fetchAppsByCategory (category) {
   return apps
 }
 
-async function fetchThirdPartyNumbers () {
-  const url = 'https://www.random.org/integers'
-  const params = {
-    num: 1,
-    min: 1,
-    max: 5,
-    col: 1,
-    base: 10,
-    format: 'plain',
-    rnd: 'new'
-  }
+async function fetchThirdPartyNumbers (options) {
+  const {
+    num = 1,
+    min = 1,
+    max = 5,
+    col = 1,
+    base = 10,
+    format = 'plain',
+    rnd = 'new'
+  } = options
+
+  const url = `https://www.random.org/integers/?num=${num}&min=${min}&max=${max}&col=${col}&base=${base}&format=${format}&rnd=${rnd}`
+
   try {
-    const res = await axios.get(url, { params })
+    const res = await axios.get(url)
     return res.data
   } catch (error) {
     logger.error('Error fetching random numbers from extarnal API', { error })
